6da14267bc145df8ab37de648ba8387529e07dbc
Pablo <pabloesquilache@gmail.com>
2022-11-06 12:33:16 -0300

    ordenando Codigo

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ import { getStock } from "./firebase.js";
 const card_container = document.getElementById("card_container");
 
 //CARRITO
-const card = document.getElementById("cart");
+//const card = document.getElementById("cart");
 const cart_container = document.getElementById("cart_modal_container");
 let carrito = JSON.parse(localStorage.getItem("cart")) || [];
 let cantidad = 1;
@@ -27,7 +27,7 @@ window.addEventListener("DOMContentLoaded", async () => {
   });
 });
 
-//LISTARCARDS
+//LISTAR CARDS
 const listar_card = (product) => {
   let card = document.createElement("div");
   card.className = "card_shop";
@@ -39,17 +39,15 @@ const listar_card = (product) => {
         <h3>${product.marca} ${product.modelo}</h3>
         <p>$ ${product.precio}</p>
       </div>
+      <div class="btn_count">
+        <button class="btn_min">-</button>
+        <span class="input_count">1</span>
+        <button class="btn_max">+</button>
+      </div>
     `;
   card_container.append(card);
-  //ITEM COUNT
-  let count_container = document.createElement("div");
-  count_container.className = "btn_count";
-  card.append(count_container);
-
-  let btn_min = document.createElement("button");
-  btn_min.className = "btn_min";
-  btn_min.innerText = "-";
-  count_container.appendChild(btn_min);
+    //ITEM COUNT
+  let btn_min = card.querySelector(".btn_min");
   btn_min.addEventListener("click", () => {
     cantidad--;
     if (cantidad <= 1) {
@@ -58,26 +56,20 @@ const listar_card = (product) => {
     input_count.innerText = cantidad;
   });
 
-  let input_count = document.createElement("span");
-  input_count.className = "input_count";
-  input_count.innerText = "1";
-  count_container.append(input_count);
+  let input_count = card.querySelector(".input_count");
 
-  let btn_max = document.createElement("button");
-  btn_max.className = "btn_max";
-  btn_max.innerText = "+";
-  count_container.appendChild(btn_max);
+  let btn_max = card.querySelector(".btn_max");
   btn_max.addEventListener("click", () => {
     cantidad++;
     input_count.innerText = cantidad;
   });
-  //BTN COMPRAR
-  let btn_comprar = document.createElement("button");
-  btn_comprar.innerText = "Añadir al carrito";
-  btn_comprar.className = "btn_comprar";
-  card.append(btn_comprar);
-  //FUNCION PARA INCORPORAR PRODUCTOS AL ARRAY DEL CARRITO
-  btn_comprar.addEventListener("click", () => {
+    //BTN AÑADIR AL CARRITO
+  let btn_add_cart = document.createElement("button");
+  btn_add_cart.innerText = "Añadir al carrito";
+  btn_add_cart.className = "btn_add_cart";
+  card.append(btn_add_cart);
+    //FUNCION PARA INCORPORAR PRODUCTOS AL ARRAY DEL CARRITO
+  btn_add_cart.addEventListener("click", () => {
     const repeat = carrito.some(
       (repeat_prduct) => repeat_prduct.id === product.id
     );
@@ -111,13 +103,11 @@ const print_cart = () => {
   let modal_header = document.createElement("div");
   modal_header.className = "modal_header";
   modal_header.innerHTML = `
-            <h1>Carrito</h1>`;
+            <h1>Carrito</h1>
+            <button class="btn_close_modal">❌</button>`;
   cart_container.append(modal_header);
   //BOTON PARA CERRAR CARRITO
-  let btn_close_modal = document.createElement("button");
-  btn_close_modal.innerText = "❌";
-  btn_close_modal.className = "btn_close_modal";
-  modal_header.append(btn_close_modal);
+  let btn_close_modal = modal_header.querySelector(".btn_close_modal");
   btn_close_modal.addEventListener("click", () => {
     cart_container.style.display = "none";
   });
@@ -137,14 +127,11 @@ const print_cart = () => {
     <p>$ ${product.precio}</p>
     <p>${product.cantidad}</p>
     <p>Subtotal: ${product.precio * product.cantidad}</p>
+    <button class="btn_delete">❌</button>
     `;
       cart_container.append(modal_body);
       //BOTON PARA ELIMINAR ELEMENTOS
-      let eliminar = document.createElement("button");
-      eliminar.innerText = "❌";
-      eliminar.className = "btn_delete";
-      modal_body.appendChild(eliminar);
-
+      let eliminar = modal_body.querySelector(".btn_delete");
       eliminar.addEventListener("click", delete_product);
     });
     //FUNCION PARA SUMAR TOTAL COMPRA
